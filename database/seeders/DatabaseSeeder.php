5b216f31ce9c1c9ac1bc2e1600f9bde4ed6b909e
JaccRk <36125321+JaccRk@users.noreply.github.com>
2021-05-22 22:56:31 -0600

    agregar seeders orden

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //especificar orden
+        $this->call(profiles::class);
+        $this->call(categories::class);
 
 
+         //ejecutar siempre en la terminal
+         //1- composer dump-autoload (para encontrar todas las actualizaciones)
+         //2- php artisan db:seed
+         //
+         // en caso de emergencia php artisan migrate:refresh --seed
+         //
     }
 }
